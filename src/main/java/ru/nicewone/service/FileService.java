85a58897de5045684b199752e5dc068df9d61c3c
Igor Prokaev <i.prokaev@rtech.ru>
2022-04-07 18:08:42 +0300

    5d

--- src/main/java/ru/nicewone/service/FileService.java
+++ src/main/java/ru/nicewone/service/FileService.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Map;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import ru.nicewone.model.DataType;
 
@@ -17,15 +17,35 @@ import ru.nicewone.model.DataType;
 public class FileService {
 
     public Void doTheJob(List<DataType> dataTypes) throws IOException {
-        Resource resource = new ClassPathResource("files-in");
-        Path pathFolder = Paths.get(resource.getFile().getAbsolutePath());
-        List<Path> pathToFileList = Files.walk(pathFolder).filter(Files::isRegularFile).toList();
+        Path inPath = Paths.get(new ClassPathResource("files-in").getFile().getAbsolutePath());
+        List<Path> pathToFileList = Files.walk(inPath).filter(Files::isRegularFile).toList();
+
         for (Path pathToFile : pathToFileList) {
-            log.info("Current File is : " + pathToFile.toString());
-            Stream<String> lines = Files.lines(pathToFile);
-            log.info("Lines in File are : " + lines.toList().toString());
+            String fileName = pathToFile.getFileName().toString();
+            log.info("Current File is : " + fileName);
+
+            Path outPath = Paths.get(new ClassPathResource("files-out").getFile().getAbsolutePath() + "/" + fileName);
+            if (!Files.exists(outPath)) {
+                Files.createFile(outPath);
+            }
+
+            for (String line : Files.lines(pathToFile).toList()) {
+                doParse(line, outPath, dataTypes);
+            }
         }
 
         return null;
+    }
+
+    private void doParse(String line, Path outPath, List<DataType> dataTypes) throws IOException {
+        for (DataType dataType : dataTypes) {
+            if (line.matches(dataType.eventRegExp())) {
+                LocalDateTime now = LocalDateTime.now();
+                Map<String, String> data = dataType.data();
+
+                Files.writeString(outPath, "My string to save");
+                System.out.println(line);
+            }
+        }
     }
 }
