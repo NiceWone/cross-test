d5ab8dc877c1bbe44f39c9931548c5021515618c
Igor Prokaev <i.prokaev@rtech.ru>
2022-04-11 14:03:45 +0300

    9

--- src/main/java/ru/nicewone/service/FileService.java
+++ src/main/java/ru/nicewone/service/FileService.java
@@ -1,23 +1,25 @@
 package ru.nicewone.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import ru.nicewone.model.DataType;
 import ru.nicewone.model.DataTypeOut;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Service
 @Log4j2
@@ -53,24 +55,32 @@ public class FileService {
     }
 
     private void doParse(String line, Path outPath, List<DataType> dataTypes) throws IOException {
+        List<String> toWriteList = new ArrayList<>(100);
         for (DataType dataType : dataTypes) {
             Pattern patternLine = Pattern.compile(dataType.eventRegExp());
             Matcher matcherLine = patternLine.matcher(line);
 
             if (matcherLine.find()) {
-                String substring = line.substring(matcherLine.start(), matcherLine.end());
                 DataTypeOut dataTypeOut = new DataTypeOut(dataType.type(), LocalDateTime.now(), new HashMap<>());
 
                 for (Entry<String, String> entry : dataType.data().entrySet()) {
                     Pattern patternSubString = Pattern.compile(entry.getValue());
-                    Matcher matcherSubString = patternSubString.matcher(substring);
+                    Matcher matcherSubString = patternSubString.matcher(line);
                     if (matcherSubString.find()) {
                         dataTypeOut.data().put(entry.getKey(), line.substring(matcherLine.start(), matcherLine.end()));
-                        Files.writeString(outPath, objectMapper.writeValueAsString(dataTypeOut));
-                        System.out.println(line);
+                        String resultLine = objectMapper.writeValueAsString(dataTypeOut);
+                        log.info("Строка : " + line);
+                        log.info("Результат: " + resultLine);
+                        toWriteList.add(resultLine);
                     }
                 }
             }
+        }
+
+        if (!toWriteList.isEmpty()) {
+            String stingToWrite =
+                    toWriteList.stream().collect(Collectors.joining(System.lineSeparator())) + System.lineSeparator();
+            Files.writeString(outPath, stingToWrite, StandardOpenOption.APPEND);
         }
     }
 }
